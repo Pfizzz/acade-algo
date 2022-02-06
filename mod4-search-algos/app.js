062e95b649e4dfad962e278a9e21dc39a410f3b5
Michael Pfister <pfiste80@gmail.com>
2022-02-06 13:10:59 -0500

    linear search

--- mod4-search-algos/app.js
+++ mod4-search-algos/app.js
@@ -20,7 +20,45 @@ function linearSearch(array, element){
 const array = [5, 3, 10, -10, 33, 51];
 
 // return the position of that value.
-console.log(linearSearch(array, 10));
-console.log(linearSearch(array, 33));
+console.log(linearSearch(array, 10)); // prints 2
+console.log(linearSearch(array, 33)); // prints 4
 
-// an array of strings would work the same way (numbers and strings are primitive values) but not objects
+// my attempt with a for loop:
+
+const array2 = [1, 5, 93, 14, 7, 23];
+
+function linSearch(array, element){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === element){
+            return i;
+        }
+    }
+}
+
+console.log(linSearch(array2, 14)); // prints 3
+console.log(linSearch(array2, 23)); // prints 5
+
+// an array of strings would work the same way (numbers and strings are primitive values) but not objects (which are reference values)
+
+// if we know the object with have a name key
+
+const players = [{ name: 'Jill', score: 100 }, { name: 'Michael', score: 100 }, { name: 'Fred', score: 90 }];
+function linearSearchObj(array, element){
+    let index = 0;
+    for(const item of array) {
+        if(
+        typeof element === 'object' &&
+        element !== null &&
+        element.name === item.name){
+            return index;
+        }
+        if(item == element){
+           return index;
+        }
+        index++;
+    }
+}
+
+console.log(linearSearchObj(players, { name: 'Michael' })); // prints 1
+
+// TIME COMPLEXITY:
